a73a27f3b7b6e430090bca6a3ebe8710593c843f
PirateKaiju <santosgonsalvesjr@gmail.com>
2019-10-20 23:54:51 -0300

    First Commit

---
+++ server.go
@@ -0,0 +1,41 @@
+package main
+
+import(
+	"os"
+	"net/http"
+	"path/filepath"
+	"fmt"
+);
+
+func main(){
+
+	var pathArg = "/var/http/";
+	var portArg = "3000";
+
+
+	if (len(os.Args) > 2){
+
+		pathArg = os.Args[1];
+		portArg = os.Args[2];
+
+	} else if (len(os.Args) > 1){
+
+		exec, err := os.Executable();
+
+		if(err != nil){
+			panic(err);
+		}
+
+		pathArg = filepath.Dir(exec);
+		portArg = os.Args[1];
+
+	}
+
+	fmt.Println("Serving: "+ pathArg);
+	
+	fs := http.FileServer(http.Dir(pathArg));
+	http.Handle("/", fs);
+
+	http.ListenAndServe(":" + portArg, nil);
+
+}
